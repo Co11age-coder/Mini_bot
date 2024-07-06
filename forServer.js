1370a04d21c628c9962a03873313eae3ec9eef1c
Yakub <yakupteuvazhukov@gmail.com>
2024-07-06 20:04:01 +0300

    ıuygu

--- forServer.js
+++ forServer.js
@@ -5,10 +5,10 @@ const port = 3000
 
 //app.use(express.static('images'))
 //app.use('/static'), express.static('images')
-app.use('/css', express.static(css))
-app.use('/js', express.static(js))
-app.use('/html', express.static(html))
-app.use('/image', express.static(images))
+app.use('/css', express.static("css"))
+app.use('/js', express.static("js"))
+app.use('/html', express.static("html"))
+app.use('/image', express.static("images"))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
