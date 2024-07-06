821828a25d0b488c5da664fbbe1d5d442d60d995
Yakub <yakupteuvazhukov@gmail.com>
2024-07-06 20:10:39 +0300

    343

--- forBot.js
+++ forBot.js
@@ -8,6 +8,6 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
-
+  
 
 bot.start();
--- forServer.js
+++ forServer.js
@@ -12,7 +12,7 @@ app.use('/image', express.static("images"))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+})  
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
