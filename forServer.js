9bfca7a806bd51991d54e6c565f47a82d420730d
Yakub <yakupteuvazhukov@gmail.com>
2024-07-09 13:04:18 +0300

    lknftcgnbhjipo[jxmpnhg ]

--- forServer.js
+++ forServer.js
@@ -11,7 +11,7 @@ app.use('/html', express.static("html"))
 app.use('/image', express.static("images"))
 
 app.get('/', (req, res) => {
-  res.send('CRYBAUTYAAAAAAAAAAAAREEEE!')
+  res.send('HelloWorld!')
 })  
 
 app.listen(port, () => {
