04df6cadcab52f6bbbb433cf68cb2c18a4bdca8e
Yakub <yakupteuvazhukov@gmail.com>
2024-07-13 17:28:55 +0300

    thıomfkdgıtrkodgfoöl

--- forBot.js
+++ forBot.js
@@ -2,7 +2,7 @@ import { Bot } from "grammy";
 
 
 const bot = new Bot("7352192152:AAGtPkRZGqvzcIyVHkEteO0iS4ti8FvNxN4");
-
+let batata =process.env.There_IS_Key
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
